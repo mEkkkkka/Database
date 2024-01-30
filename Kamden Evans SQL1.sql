1b75b3e2ce1b27ad8c8d98af6bbe4984e6e723b6
mEkkkkka <code.kam6@gmail.com>
2024-01-30 12:22:38 -0700

    Added stubs for each question

--- Kamden Evans SQL1.sql
+++ Kamden Evans SQL1.sql
@@ -1,3 +1,74 @@
 -- Kamden Evans
 -- SQL 1
 -- 01/30/2024
+
+
+-- 1
+SELECT
+    prodPrice
+FROM
+    product
+;
+
+-- 2
+SELECT 
+    ordCity
+FROM 
+    orderTbl
+;
+
+-- 3
+SELECT
+    custLastName
+FROM
+    customer
+;
+
+-- 4
+SELECT
+    firstName
+FROM
+    students
+;
+
+-- 5
+SELECT
+    empNo
+FROM
+    orderTbl
+;
+
+-- 6
+SELECT
+    buildingName
+FROM
+    buildings
+;
+
+-- 7
+SELECT
+    capacity
+FROM
+    sections
+;
+
+-- 8
+SELECT
+    professorId
+FROM
+    professors
+;
+
+-- 9
+SELECT
+    assignmentNumber
+FROM
+    assignmentScore
+;
+
+-- 10
+SELECT
+    ordCity
+FROM
+    orderTbl
+;
