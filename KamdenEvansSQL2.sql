6c9bd3ca815fb3f56a312e74880f00249728d9bc
mEkkkkka <code.kam6@gmail.com>
2024-02-05 12:13:28 -0700

    Created file and stubs for SQL 2

---
+++ KamdenEvansSQL2.sql
@@ -0,0 +1,73 @@
+-- Kamden Evans
+-- SQL 2
+-- 02/05/2024
+
+-- 1
+SELECT
+    firstName || ' ' || lastName AS full_name, phone AS phone, zip AS zip
+FROM
+    students
+-- TBD
+
+-- 2
+SELECT
+    firstName || ' ' || lastName AS full_name, phone AS phone, tuitionBalance AS balance
+FROM
+    students
+-- TBD
+
+-- 3
+SELECT
+    firstName AS student_name, '*******' || phone AS phone, streetAddress AS street
+FROM
+    students
+-- TBD
+
+-- 4
+SELECT
+    studentID AS id, tuitionBalance AS balance
+FROM
+    students
+-- TBD
+
+-- 5
+SELECT DISTINCT
+    zip as zip
+FROM
+    students
+-- TBD
+
+-- 6
+SELECT
+    studentID AS student_id, score AS score
+FROM
+    assignmentScore
+-- TBD
+
+-- 7
+SELECT
+    state AS state_name, state AS state, city AS city
+FROM
+    professors
+-- TBD
+
+-- 8
+SELECT
+    firstName || ' ' || lastName || ' ' || streetAddress || ' ' || city || ' ' || state || ' ' || zip AS student_address
+FROM
+    students
+-- TBD
+
+-- 9
+SELECT
+    studentID AS student_id, score AS score, sectionID AS section_id
+FROM
+    assignmentScore
+-- TBD
+
+-- 10
+SELECT
+    AS full_name, AS favorite_food, AS favorite_number, AS squared, AS length_of_name
+FROM
+    --A new table
+-- TBD
