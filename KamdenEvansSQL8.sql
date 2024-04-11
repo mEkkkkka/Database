93ea11de03b14c98fb1afedeb7ec32851b9c44f5
mEkkkkka <code.kam6@gmail.com>
2024-04-11 12:51:42 -0600

    A bunch of work on q1, some work on q8

--- KamdenEvansSQL8.sql
+++ KamdenEvansSQL8.sql
@@ -4,10 +4,40 @@
 
 -- 1
 SELECT
-    stu.firstName,
-    stu.lastName
+    stu.firstName AS first_name,
+    stu.lastName AS last_name,
+    'Student' AS role
 FROM
     students stu
+WHERE
+    TO_CHAR(stu.admissionDate, 'DD, MM, fmMonth') = 
+    (
+        SELECT
+            MIN(TO_CHAR(stu.admissionDate, 'DD, MM, fmMonth')) AS earliest_day
+        FROM
+            students stu
+    )
+
+UNION
+
+SELECT
+    prof.firstName AS first_name,
+    prof.lastName AS last_name,
+    'Professor' AS role
+FROM
+    professors prof
+WHERE
+    TO_CHAR(prof.hireDate, 'DD, MM, fmMonth') = 
+    (
+        SELECT
+            MAX(TO_CHAR(prof.hireDate, 'DD, MM, fmMonth')) AS latest_day
+        FROM
+            professors prof
+    )
+
+ORDER BY
+    2 ASC,
+    1 ASC
 ;
 --TBD
 
@@ -65,9 +95,16 @@ FROM
 
 -- 8
 SELECT
-    cou.subjectCode || ' ' || cou.courseNumber
+    
+    sec.sectionID AS section_id,
+    cou.subjectCode || ' ' || cou.courseNumber AS course_info,
+    sec.capacity AS capacity
 FROM
     courses cou
+JOIN
+    sections sec
+ON
+    cou.courseID = sec.sectionID
 ;
 --TBD
 
