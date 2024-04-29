e57a95431964cf1d038d3b1438a6a494bb4b40bc
mEkkkkka <code.kam6@gmail.com>
2024-04-29 12:24:38 -0600

    Filled in the rest of the tables

--- KamdenEvansCreateTable.sql
+++ KamdenEvansCreateTable.sql
@@ -5,36 +5,54 @@
 -- Creating table albums
 CREATE TABLE albums
 (
-    album_id NUMBER(3) PRIMARY KEY
+    album_id NUMBER(3) PRIMARY KEY,
+    title VARCHAR2(160) NOT NULL,
+    artist_id NUMBER(3) NOT NULL,
+    year DATE
 );
 -- TBD
 
 -- Creating table artists
 CREATE TABLE artists
 (
-    artist_id NUMBER(3) PRIMARY KEY
+    artist_id NUMBER(3) PRIMARY KEY,
+    name VARCHAR2(120) NOT NULL
 );
 -- TBD
 
 -- Creating table genres
 CREATE TABLE genres
 (
-    genre_id NUMBER(3) PRIMARY KEY
+    genre_id NUMBER(3) PRIMARY KEY,
+    name VARCHAR2(120) NOT NULL
 );
 -- TBD
 
 -- Creating table media_types
 CREATE TABLE media_types
 (
-    media_type_id NUMBER(3) PRIMARY KEY
+    media_type_id NUMBER(3) PRIMARY KEY,
+    name VARCHAR2(120) NOT NULL
 );
 -- TBD
 
 -- Creating table tracks
 CREATE TABLE tracks
 (
-    track_id NUMBER(5) PRIMARY KEY
-)
+    track_id NUMBER(5) PRIMARY KEY,
+    name VARCHAR2(200) NOT NULL,
+    album_id NUMBER(3) NOT NULL,
+    artist_id NUMBER(3),
+    media_type_id NUMBER(3) NOT NULL,
+    genre_id NUMBER(3) NOT NULL,
+    composer VARCHAR2(220),
+    milliseconds NUMBER(10) NOT NULL,
+    bytes NUMBER(10) NOT NULL,
+    unit_price NUMBER(10,2) NOT NULL,
+    play_count NUMBER(3) NOT NULL,
+    skip_count NUMBER(4) NOT NULL,
+    track_index NUMBER(3)
+);
 -- TBD
 
 -- Creating foreign keys
