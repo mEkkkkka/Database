7beb5111b6b469ca2005731771fb0a40e22bcbd6
mEkkkkka <code.kam6@gmail.com>
2024-04-29 12:15:44 -0600

    Stubbed all required stubs

--- KamdenEvansCreateTable.sql
+++ KamdenEvansCreateTable.sql
@@ -3,23 +3,38 @@
 -- 04/29/2024
 
 -- Creating table albums
-
+CREATE TABLE albums
+(
+    album_id NUMBER(3) PRIMARY KEY
+);
 -- TBD
 
 -- Creating table artists
-
+CREATE TABLE artists
+(
+    artist_id NUMBER(3) PRIMARY KEY
+);
 -- TBD
 
 -- Creating table genres
-
+CREATE TABLE genres
+(
+    genre_id NUMBER(3) PRIMARY KEY
+);
 -- TBD
 
 -- Creating table media_types
-
+CREATE TABLE media_types
+(
+    media_type_id NUMBER(3) PRIMARY KEY
+);
 -- TBD
 
 -- Creating table tracks
-
+CREATE TABLE tracks
+(
+    track_id NUMBER(5) PRIMARY KEY
+)
 -- TBD
 
 -- Creating foreign keys
