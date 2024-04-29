f8dd69a4c049633020f9a61d49d268cad2b0e644
mEkkkkka <code.kam6@gmail.com>
2024-04-29 12:05:26 -0600

    Created CreateTable project and commented sections

---
+++ KamdenEvansCreateTable.sql
@@ -0,0 +1,47 @@
+-- Kamden Evans
+-- SQL 11
+-- 04/29/2024
+
+-- Creating table albums
+
+-- TBD
+
+-- Creating table artists
+
+-- TBD
+
+-- Creating table genres
+
+-- TBD
+
+-- Creating table media_types
+
+-- TBD
+
+-- Creating table tracks
+
+-- TBD
+
+-- Creating foreign keys
+
+-- TBD
+
+-- Inserting data into table albums
+
+-- TBD
+
+-- Inserting data into table artists
+
+-- TBD
+
+-- Inserting data into table genres
+
+-- TBD
+
+-- Inserting data into table media_types
+
+-- TBD
+
+-- Inserting data into table tracks
+
+-- TBD
