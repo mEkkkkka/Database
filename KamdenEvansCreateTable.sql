406f20f9cdf25f44e172d6fb62550f64f76f3bf5
mEkkkkka <code.kam6@gmail.com>
2024-05-01 12:10:06 -0600

    Finalized the assignment; updated status messages and fixed ambiguous column errors in track insert

--- KamdenEvansCreateTable.sql
+++ KamdenEvansCreateTable.sql
@@ -121,7 +121,7 @@ ON DELETE
 -- IMPORTANT: ALL BLANK NEED NULL
 
 -- Inserting data into table albums
-INSERT INTO albums (album_id, title, artist_id, year)
+INSERT INTO albums (album_id, title, year, artist_id)
 WITH album_stuff AS
 (
     SELECT 74, 'The Bog / I-Q', NULL, 12 FROM dual UNION ALL
@@ -153,7 +153,7 @@ SELECT
 FROM
     album_stuff
 ;
--- Check this one
+-- This one is done
 
 -- Inserting data into table artists
 INSERT INTO artists (artist_id, name)
@@ -187,7 +187,7 @@ SELECT
 FROM
     artist_stuff
 ;
--- Check this one
+-- This one is done
 
 -- Inserting data into table genres
 INSERT INTO genres (genre_id, name)
@@ -221,7 +221,7 @@ SELECT
 FROM
     genre_stuff
 ;
--- Check this one
+-- This one is done
 
 -- Inserting data into table media_types
 INSERT INTO media_types (media_type_id, name)
@@ -232,45 +232,45 @@ WITH media_type_stuff AS
     SELECT 3, 'AAC audio file' FROM dual UNION ALL
     SELECT 4, 'Purchased AAC audio file' FROM dual UNION ALL
     SELECT 5, 'PDF document' FROM dual UNION ALL
-    SELECT 6, 'Protected AAC audio file' FROM
+    SELECT 6, 'Protected AAC audio file' FROM dual
 )
 SELECT
     *
 FROM
     media_type_stuff
 ;
--- Check this one
+-- This one is done
 
 -- Inserting data into table tracks
-INSERT INTO tracks (track_id, title, milliseconds, artist_id, album_id, genre_id, play_count, skip_count, composer, track_index, bytes, unit_price, media_type_id)
+INSERT INTO tracks (track_id, name, milliseconds, artist_id, album_id, genre_id, play_count, skip_count, composer, track_index, bytes, unit_price, media_type_id)
 WITH track_stuff AS
 (
-    SELECT 18, 'Hurt', 219000, 65, 632, 11, 32, 0, NULL, 2, 3879731, 0, 1 FROM dual UNION ALL
-    SELECT 24, 'Make You Smile', 225000, 28, 89, 3, 81, 3, NULL, NULL, 1992294, 0.99, 4 FROM dual UNION ALL
-    SELECT 25, 'La Migra (Mas Espanol)', 174000, 55, 348, 3, 55, 0, NULL, 12, 2936013, 0.99, 4 FROM dual UNION ALL
-    SELECT 34, 'Stars Come Out (Dillon Francis Remix)', 249000, 737, 200, 12, 5, 1, NULL, 1, 6291456, 0, 3 FROM dual UNION ALL
-    SELECT 37, 'Christmas Eve Montage', 226000, 22, 85, 6, 15, 0, NULL, 15, 7864320, 0, 3 FROM dual UNION ALL
-    SELECT 60, 'Please Don''t Leave Me', 232000, 38, 176, 2, 1, 2, 'P!nk &' || ' Max Martin', 5, 8703181, 0.99, 4 FROM dual UNION ALL
-    SELECT 148, 'Shiver', 202000, 733, 192, 21, 12, 1, NULL, 1, 3460301, 0, 1 FROM dual UNION ALL
-    SELECT 171, 'Promises', 257000, 119, 178, 14, 5, 0, NULL, 1, 8703181, 0, 1 FROM dual UNION ALL
-    SELECT 184, 'Don''t Let Me Get Me (Radio Edit)', 211000, 38, 332, 2, 0, 1, 'P!nk &' || ' Dallas Austin', 2, 8178893, 0.99, 6 FROM dual UNION ALL
-    SELECT 202, 'Running Out Of Angels (Demo)', 125000, 127, 186, 3, 69, 0, NULL, NULL, 1153434, 0.99, 4 FROM dual UNION ALL
-    SELECT 236, 'Tempt My Trouble', 194000, 48, 342, 4, 8, 0, NULL, 1, 8283750, 1.99, 4 FROM dual UNION ALL
-    SELECT 289, 'White Horse', 234000, 97, 443, 11, 0, 3, NULL, 5, 8703181, 0.99, 4 FROM dual UNION ALL
-    SELECT 301, 'The Bog (Radio Mix)', 243000, 12, 74, 5, 70, 0, NULL, NULL, 2097152, 0, 1 FROM dual UNION ALL
-    SELECT 344, 'Seether', 197000, 732, 191, 3, 13, 0, 'Nina Gordon', 3, 3670016, 0, 1 FROM dual UNION ALL
-    SELECT 346, 'Untouched', 255000, 752, 435, 2, 42, 0, 'Toby Gad, Jessica Origliasso &' || ' Lisa Origliasso', 1, 4508877, 0.99, 6 FROM dual UNION ALL
-    SELECT 390, '!Purple (Sasha v. The Light)', 495000, 120, 179, 19, 60, 1, NULL, NULL, 8283750, 0, 1 FROM dual UNION ALL
-    SELECT 391, 'Feel Good Inc (Single Edit)', 221000, 46, 340, 4, 54, 0, NULL, 1, 3774874, 0.99, 6 FROM dual UNION ALL
-    SELECT 759, 'Charlotte Sometimes (Single)', 253000, 13, 84, 3, 63, 0, NULL, NULL, 2202010, 0, 1 FROM dual UNION ALL
-    SELECT 788, 'Solsbury Hill', 259000, 98, 685, 8, 66, 0, NULL, NULL, 2306867, 0, 1 FROM dual UNION ALL
-    SELECT 798, 'For A Pessimist, I''m Pretty Optimistic', 217000, 32, 93, 3, 68, 0, NULL, NULL, 1992294, 0.99, 4 FROM dual UNION ALL
-    SELECT 31611, 'Soul Meets Body', 231000, 95, 198, 3, 65, 0, NULL, NULL, 1992294, 0, 1 FROM dual UNION ALL
-    SELECT 43173, 'Twas the Night Before Xmas', 248000, 126, 185, 3, 1, 0, NULL, NULL, 4194304, 0, 1 FROM dual
+    SELECT 18 AS track_id, 'Hurt' AS title, 219000 AS milliseconds, 65 AS artist_id, 632 AS album_id, 11 AS genre_id, 32 AS play_count, 0 AS skip_count, NULL AS composer, 2 AS track_index, 3879731 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 24 AS track_id, 'Make You Smile' AS title, 225000 AS milliseconds, 28 AS artist_id, 89 AS album_id, 3 AS genre_id, 81 AS play_count, 3 AS skip_count, NULL AS composer, NULL AS track_index, 1992294 AS bytes, 0.99 AS unit_price, 4 AS media_type_id FROM dual UNION ALL
+    SELECT 25 AS track_id, 'La Migra (Mas Espanol)' AS title, 174000 AS milliseconds, 55 AS artist_id, 348 AS album_id, 3 AS genre_id, 55 AS play_count, 0 AS skip_count, NULL AS composer, 12 AS track_index, 2936013 AS bytes, 0.99 AS unit_price, 4 AS media_type_id FROM dual UNION ALL
+    SELECT 34 AS track_id, 'Stars Come Out (Dillon Francis Remix)' AS title, 249000 AS milliseconds, 737 AS artist_id, 200 AS album_id, 12 AS genre_id, 5 AS play_count, 1 AS skip_count, NULL AS composer, 1 AS track_index, 6291456 AS bytes, 0 AS unit_price, 3 AS media_type_id FROM dual UNION ALL
+    SELECT 37 AS track_id, 'Christmas Eve Montage' AS title, 226000 AS milliseconds, 22 AS artist_id, 85 AS album_id, 6 AS genre_id, 15 AS play_count, 0 AS skip_count, NULL AS composer, 15 AS track_index, 7864320 AS bytes, 0 AS unit_price, 3 AS media_type_id FROM dual UNION ALL
+    SELECT 60 AS track_id, 'Please Don''t Leave Me' AS title, 232000 AS milliseconds, 38 AS artist_id, 176 AS album_id, 2 AS genre_id, 1 AS play_count, 2 AS skip_count, 'P!nk &' || ' Max Martin' AS composer, 5 AS track_index, 8703181 AS bytes, 0.99 AS unit_price, 4 AS media_type_id FROM dual UNION ALL
+    SELECT 148 AS track_id, 'Shiver' AS title, 202000 AS milliseconds, 733 AS artist_id, 192 AS album_id, 21 AS genre_id, 12 AS play_count, 1 AS skip_count, NULL AS composer, 1 AS track_index, 3460301 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 171 AS track_id, 'Promises', 257000 AS milliseconds, 119 AS artist_id, 178 AS album_id, 14 AS genre_id, 5 AS play_count, 0 AS skip_count, NULL AS composer, 1 AS track_index, 8703181 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 184 AS track_id, 'Don''t Let Me Get Me (Radio Edit)' AS title, 211000 AS milliseconds, 38 AS artist_id, 332 AS album_id, 2 AS genre_id, 0 AS play_count, 1 AS skip_count, 'P!nk &' || ' Dallas Austin' AS composer, 2 AS track_index, 8178893 AS bytes, 0.99 AS unit_price, 6 AS media_type_id FROM dual UNION ALL
+    SELECT 202 AS track_id, 'Running Out Of Angels (Demo)' AS title, 125000 AS milliseconds, 127 AS artist_id, 186 AS album_id, 3 AS genre_id, 69 AS play_count, 0 AS skip_count, NULL AS composer, NULL AS track_index, 1153434 AS bytes, 0.99 AS unit_price, 4 AS media_type_id FROM dual UNION ALL
+    SELECT 236 AS track_id, 'Tempt My Trouble' AS title, 194000 AS milliseconds, 48 AS artist_id, 342 AS album_id, 4 AS genre_id, 8 AS play_count, 0 AS skip_count, NULL AS composer, 1 AS track_index, 8283750 AS bytes, 1.99 AS unit_price, 4 AS media_type_id FROM dual UNION ALL
+    SELECT 289 AS track_id, 'White Horse' AS title, 234000 AS milliseconds, 97 AS artist_id, 443 AS album_id, 11 AS genre_id, 0 AS play_count, 3 AS skip_count, NULL AS composer, 5 AS track_index, 8703181 AS bytes, 0.99 AS unit_price, 4 AS media_type_id FROM dual UNION ALL
+    SELECT 301 AS track_id, 'The Bog (Radio Mix)' AS title, 243000 AS milliseconds, 12 AS artist_id, 74 AS album_id, 5 AS genre_id, 70 AS play_count, 0 AS skip_count, NULL AS composer, NULL AS track_index, 2097152 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 344 AS track_id, 'Seether' AS title, 197000 AS milliseconds, 732 AS artist_id, 191 AS album_id, 3 AS genre_id, 13 AS play_count, 0 AS skip_count, 'Nina Gordon' AS composer, 3 AS track_index, 3670016 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 346 AS track_id, 'Untouched' AS title, 255000 AS milliseconds, 752 AS artist_id, 435 AS album_id, 2 AS genre_id, 42 AS play_count, 0 AS skip_count, 'Toby Gad, Jessica Origliasso &' || ' Lisa Origliasso' AS composer, 1 AS track_index, 4508877 AS bytes, 0.99 AS unit_price, 6 AS media_type_id FROM dual UNION ALL
+    SELECT 390 AS track_id, '!Purple (Sasha v. The Light)' AS title, 495000 AS milliseconds, 120 AS artist_id, 179 AS album_id, 19 AS genre_id, 60 AS play_count, 1 AS skip_count, NULL AS composer, NULL AS track_index, 8283750 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 391 AS track_id, 'Feel Good Inc (Single Edit)' AS title, 221000 AS milliseconds, 46 AS artist_id, 340 AS album_id, 4 AS genre_id, 54 AS play_count, 0 AS skip_count, NULL AS composer, 1 AS track_index, 3774874 AS bytes, 0.99 AS unit_price, 6 AS media_type_id FROM dual UNION ALL
+    SELECT 759 AS track_id, 'Charlotte Sometimes (Single)' AS title, 253000 AS milliseconds, 13 AS artist_id, 84 AS album_id, 3 AS genre_id, 63 AS play_count, 0 AS skip_count, NULL AS composer, NULL AS track_index, 2202010 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 788 AS track_id, 'Solsbury Hill' AS title, 259000 AS milliseconds, 98 AS artist_id, 685 AS album_id, 8 AS genre_id, 66 AS play_count, 0 AS skip_count, NULL AS composer, NULL AS track_index, 2306867 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 798 AS track_id, 'For A Pessimist, I''m Pretty Optimistic' AS title, 217000 AS milliseconds, 32 AS artist_id, 93 AS album_id, 3 AS genre_id, 68 AS play_count, 0 AS skip_count, NULL AS composer, NULL AS track_index, 1992294 AS bytes, 0.99 AS unit_price, 4 AS media_type_id FROM dual UNION ALL
+    SELECT 31611 AS track_id, 'Soul Meets Body' AS title, 231000 AS milliseconds, 95 AS artist_id, 198 AS album_id, 3 AS genre_id, 65 AS play_count, 0 AS skip_count, NULL AS composer, NULL AS track_index, 1992294 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual UNION ALL
+    SELECT 43173 AS track_id, 'Twas the Night Before Xmas' AS title, 248000 AS milliseconds, 126 AS artist_id, 185 AS album_id, 3 AS genre_id, 1 AS play_count, 0 AS skip_count, NULL AS composer, NULL AS track_index, 4194304 AS bytes, 0 AS unit_price, 1 AS media_type_id FROM dual
 )
 SELECT
     *
 FROM
     track_stuff
 ;
--- Check this one
+-- This one is done
